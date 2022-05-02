e3c939aedf0a3d17e05c56105052c1a55eb858a6
Crona Xu <cx640@nyu.edu>
2022-05-02 19:02:11 -0400

    Inventory UI complete

---
+++ Assets/Scripts/ItemData_SO.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:785cd5f32c306ad898129d1bce720880f0273aebbc129648d72b9e9e2c20d5bf
+size 295
--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6186491b282f74ee3dfadbb77a429aca2de577ce2adda2df799ba05c0f03d69a
-size 1294
+oid sha256:79e40a6ece077d21e20bab3810eb9109be83d558f7c9f05938dd96452b7bbc2d
+size 1288
